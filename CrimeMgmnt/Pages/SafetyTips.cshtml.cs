222f7808226032f1eaecc798f2e3a3905188b850
ShubhamSingh <sp638733@gmail.com>
2022-08-25 10:09:33 +0530

    Added the safety page

---
+++ CrimeMgmnt/Pages/SafetyTips.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrimeMgmnt.Pages
+{
+    public class SafetyTipsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
